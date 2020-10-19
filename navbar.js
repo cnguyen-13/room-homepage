50c95a73b0b775cf252cf8dc2b52a40c240cf405
Christian <kpnguyen1013@gmail.com>
2020-10-19 07:55:00 -0500

    Mobile view complete

---
+++ navbar.js
@@ -0,0 +1,14 @@
+const navButtons = document.querySelectorAll('.nav-bar-clickable');
+const navLinks = document.querySelector('.nav-list');
+const mask = document.querySelector('.mask');
+
+function toggleNavActive() {
+    navLinks.classList.toggle('nav-active');
+    mask.classList.toggle('mask-active');
+}
+
+navButtons.forEach((button) => {
+    button.addEventListener('click', toggleNavActive);
+});
+
+mask.addEventListener('click', toggleNavActive);
