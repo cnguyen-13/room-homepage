ea9a0c94335a21f0955ae2815b123a954a1183ef
Christian <kpnguyen1013@gmail.com>
2020-10-30 13:12:55 -0500

    Slide hero complete for mobile and dt

--- javascript/sliding.js
+++ javascript/sliding.js
@@ -1,35 +1,45 @@
 let slideNumber = 0;
 let numberOfItems = null;
-let multiplier = null;
-const left = document.querySelector('#left');
-const right = document.querySelector('#right');
-const slider = document.querySelector('#slider');
+let slideInterval = null;
+const left = document.querySelector('#slide-show-next-btn');
+const right = document.querySelector('#slide-show-prev-btn');
+const slideShows = document.querySelectorAll('.slide-show');
 
-window.addEventListener('load', () => {
+function setWidthAndInterval() {
     //Set the number of children in the slider container and interval of shifts
-    numberOfItems = document.querySelectorAll('.item').length;
-    multiplier = 100 / numberOfItems;
-
+    numberOfItems = document.querySelectorAll('.slide-show-item').length;
+    slideInterval = 100 / numberOfItems;
+    
     //Set width of slider to hold the number of items/children
     const slideWidthPercentage = numberOfItems * 100;
-    slider.style.width = `${slideWidthPercentage}%`
-})
+    slideShows.forEach(show => {
+        show.style.width = `${slideWidthPercentage}%`;
+    })
+    return;
+}
 
-function slideGallery(e) {
-    const direction = e.target.id;
-    if(direction === 'right') {
+function moveSlideShow(e) {
+    //Function slides to the next/previous image
+    const btn = e.target.id;
+    if(btn === 'slide-show-next-btn') {
+        //Prevents overflow
         if (slideNumber + 1 < numberOfItems) {
             slideNumber++;
         }
     } else {
+        //Prevents underflow
         if (slideNumber - 1 >= 0) {
             slideNumber--;
         }
     }
-
-    slider.style.transform = `translate(-${slideNumber*multiplier}%)`;
+    slideShows.forEach(show => {
+        show.style.transform = `translate(-${slideNumber*slideInterval}%)`;
+    })
     return;
 }
 
-right.addEventListener('click', slideGallery);
-left.addEventListener('click', slideGallery);
+
+//Event Listeners
+window.addEventListener('load', setWidthAndInterval);
+right.addEventListener('click', moveSlideShow);
+left.addEventListener('click', moveSlideShow);
